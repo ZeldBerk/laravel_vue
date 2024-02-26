23c7c987ceb6b82a2ff6e2d91c4d37f70edaf875
Mist3r20 <edgarnosfp@ibf.cat>
2024-02-26 19:49:53 +0100

    Create Table ingredientes_recetas y plan_semanal

---
+++ app/Models/ingredientes_recetas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ingredientes_recetas extends Model
+{
+    use HasFactory;
+}
