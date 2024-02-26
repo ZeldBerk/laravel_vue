cb204f9eb9c43f88fb0541f38cde33fe1640a4c1
Mist3r20 <edgarnosfp@ibf.cat>
2024-02-26 20:45:23 +0100

    Create Seeder Categorias

---
+++ database/seeders/CreateCategorias.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\categorias;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CreateCategorias extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categoria1 = categorias::create([
+           'nombre' => 'Primeros' 
+        ]);
+
+        $categoria2 = categorias::create([
+            'nombre' => 'Segundos'
+        ]);
+
+        $categoria3 = categorias::create([
+            'nombre' => 'Postres'
+        ]);
+    }
+}
