d4ec6aa8b84bb16df2f51cf1f7637b2bef9bd569
Pau <paust2001@gmail.com>
2024-03-01 18:30:42 +0100

    Añadir recetas (WIP)

--- resources/js/views/recetas/create.vue
+++ resources/js/views/recetas/create.vue
@@ -1,76 +1,86 @@
 <template>
     <div id="recetas" class="container">
-    <div class="card">
-        <div class="card-body">
-            <div class="d-flex justify-content-between pb-2 mb-2">
-                <h5 class="card-title">Añade una nueva receta</h5>
-            </div>
-            <div v-if="stringSuccess" class="alert alert-success alert-dismissible fade show" role="alert">
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                <strong>{{ stringSuccess }}</strong>
-            </div>
-            <div v-if="stringError" class="alert alert-danger alert-dismissible fade show" role="alert">
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                <strong>{{ stringError }}</strong>
-            </div>
-            <form @submit.prevent="addReceta">
-                <div class="row my-5">
-                    <div class="col-md-8">
-                        <div class="card border-0 shadow-sm">
-                            <div class="card-body">
-                                <!-- Nombre receta -->
-                                <div class="mb-3">
-                                    <label for="receta_nombre" class="form-label">Nombre de la receta</label>
-                                    <input v-model="receta.nombre" id="receta_nombre" type="text" class="form-control">
-                                </div>
-                                <!-- Contenido receta -->
-                                <div class="mb-3">
-                                    <label for="receta_descripcion" class="form-label">Contenido de la receta</label>
-                                    <TextEditorComponent v-model="receta.descripcion"/>
-                                </div>
+        <div class="d-flex justify-content-between pb-2 mb-2">
+            <h5 class="card-title">Añade una nueva receta</h5>
+        </div>
+        <form @submit.prevent="addReceta">
+            <div class="row my-5">
+                <div class="col-md-8">
+                    <div class="card border-0 shadow-sm">
+                        <div class="card-body">
+                            <!-- Nombre receta -->
+                            <div class="mb-3">
+                                <label for="receta_nombre" class="form-label">Nombre de la receta:</label>
+                                <input v-model="receta.nombre" id="receta_nombre" type="text" class="form-control">
                             </div>
+                            <!-- Contenido receta -->
+                            <div class="mb-3">
+                                <label for="receta_descripcion" class="form-label">Pasos de la receta</label>
+                                <TextEditorComponent v-model="receta.descripcion" />
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-4 mb-4">Añadir receta</button>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="card border-0 shadow-sm">
-                            <div class="card-body">
-                                <div class="mb-3">
-                                    <label for="receta_raciones" class="form-label">Raciones de la receta</label>
-                                    <input v-model="receta.raciones" id="receta_raciones" type="number" class="form-control">
-                                </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card border-0 shadow-sm">
+                        <div class="card-body">
+                            <!-- Raciones receta -->
+                            <div class="mb-3">
+                                <label for="receta_raciones" class="form-label">Raciones</label>
+                                <input v-model="receta.raciones" id="receta_raciones" type="number" min="1" class="form-control">
+                            </div>
+                            <!-- Tiempo receta -->
+                            <div class="mb-3">
+                                <label for="receta_tiempo_preparacion" class="form-label">Tiempo de preparación</label>
+                                <input v-model="receta.tiempo_preparacion" id="receta_tiempo_preparacion" type="number" min="1" class="form-control">
+                            </div>
+                            <!-- Categoria receta -->
+                            <div class="mb-3">
+                                <label for="receta_categoria_id" class="form-label">Selecciona la categotia</label>
+                                <input v-model="receta.categoria_id" id="receta_categoria_id" type="number" min="1" max="3" class="form-control">
+                            </div>
+                            <!-- Imagen receta -->
+                            <div class="mb-3">
+                                <label for="receta_ruta_imagen" class="form-label">Imagen</label>
+                                <input v-model="receta.ruta_imagen" id="receta_ruta_imagen" type="text" class="form-control">
+                            </div>
+                            <!-- User receta -->
+                            <div class="mb-3">
+                                <label for="receta_user_id" class="form-label">User_id = 1</label>
+                                <input v-model="receta.user_id" id="receta_user_id" type="number" class="form-control">
                             </div>
                         </div>
                     </div>
                 </div>
-            </form>
-        </div>
+            </div>
+        </form>
     </div>
-</div>
 </template>
 
 
 <script setup>
-    import { ref } from "vue";
-    import TextEditorComponent from "@/components/TextEditorComponent.vue";
+import { ref, inject } from "vue";
+import TextEditorComponent from "@/components/TextEditorComponent.vue";
 
-    const receta = ref({});
+const receta = ref({});
+const swal = inject('$swal');
 
-    const stringError = ref();
-    const stringSuccess = ref();
+function addReceta() {
 
-    function addTask(){
-
-        axios.post('/api/recetas', receta.value)
-        .then(response =>{
-            console.log(response);
-            stringSuccess.value = response.data.success;
-            stringError.value = "";
+    axios.post('/api/recetas', receta.value)
+        .then(response => {
+            swal({
+                icon: 'success',
+                title: 'Receta añadida correctamente'
+            });
         })
         .catch(error => {
-            console.log(error);
-            stringSuccess.value = "";
-            stringError.value = error.response.data.message;
+            swal({
+                icon: 'error',
+                title: 'No se ha añadido la receta'
+            });
         });
-    }
-    
+}
+
 </script>
