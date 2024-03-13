1d2c9f85055871fe8f2b8b7ae0ec038eb69cf97d
Mist3r20 <edgarnosfp@ibf.cat>
2024-03-13 19:19:37 +0100

    PlanSemanal(WIP)

--- resources/js/views/planSemanal/index.vue
+++ resources/js/views/planSemanal/index.vue
@@ -1,20 +1,32 @@
 <template>
-    <div id="planSemanal" class="container">
-        <h1>Plan Semanal</h1>
-        
-      
+    <h1>Plan Semanal</h1>
+    <div class="container card">
+      <table>
+        <tr>
+            <th class="separador espacio"></th>
+            <th class="separador espacio">Almuerzo</th>
+            <th>Cena</th>
+        </tr>
+        <tr v-for="dia in diasSemana" :key="dia">
+            <th class="separador espacio">{{ dia }}</th>
+            <td class="separador">fsdfsf</td>
+            <td></td>
+        </tr>
+      </table>
     </div>
-    
-</template>
-    
-<script>
-    import axios from "axios";
-    import { ref, onMounted } from "vue";
-    
-    export default {
-        setup() {
-      
-        }
-    }
-</script>
-    
+  </template>
+  
+  <script>
+  import { ref } from "vue";
+  
+  export default {
+    setup() {
+      const diasSemana = ref([
+        'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'
+      ]);
+  
+      return { diasSemana };
+    },
+  };
+  </script>
+  
