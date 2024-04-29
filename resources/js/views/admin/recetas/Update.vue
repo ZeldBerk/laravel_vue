ba063522b48a93a29b08cc3ffc3dc720e556a67d
ZeldBerk <paust2001@gmail.com>
2024-04-29 12:39:03 +0200

    Eliminar ingredientes nuevos en update

--- resources/js/views/admin/recetas/Update.vue
+++ resources/js/views/admin/recetas/Update.vue
@@ -39,8 +39,20 @@
                                             <option value="L">Litros</option>
                                             <option value="unidades">Unidades</option>
                                         </select>
-                                        <span class="delete-icon" @click.stop="deleteIngredienteRecetas(ingrediente.id)">
-                                            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
+                                        <span class="delete-icon"
+                                            @click.stop="deleteIngredienteRecetas(ingrediente.id)">
+                                            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg" stroke="#000000">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
+                                                    stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <path
+                                                        d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6"
+                                                        stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                                        stroke-linejoin="round"></path>
+                                                </g>
+                                            </svg>
                                         </span>
                                     </div>
                                 </div>
@@ -306,9 +318,39 @@ function ingrediente_selection(id_selection) {
             nuevoIngrediente.unidad = event.target.value;
         });
 
+        // Crear el elemento span
+        const spanElement = document.createElement('span');
+        spanElement.setAttribute('class', 'delete-icon');
+        spanElement.addEventListener('click', event => {
+            deleteIngredienteRecetas_nuevos(nuevoIngrediente.id);
+            // Eliminar visualmente el contenedor del ingrediente
+            flexContainer.remove();
+        });
+
+        // Crear el elemento SVG dentro del span
+        const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svgElement.setAttribute("width", "25px");
+        svgElement.setAttribute("height", "25px");
+        svgElement.setAttribute("viewBox", "0 0 24 24");
+        svgElement.setAttribute("fill", "none");
+        svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svgElement.innerHTML = `
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+                <path
+                    d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6"
+                    stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                </path>
+            </g>`;
+
+        // Agregar el elemento SVG al span
+        spanElement.appendChild(svgElement);
+
         // Insertar los inputs en el contenedor flex
         flexContainer.appendChild(cantidadInput);
         flexContainer.appendChild(unidadSelect);
+        flexContainer.appendChild(spanElement);
 
         // Insertar el contenedor flex en el contenedor principal
         ingredientContainer.appendChild(flexContainer);
@@ -337,6 +379,18 @@ function deleteIngredienteRecetas(ingredienteId) {
                 title: 'No se pudo eliminar el ingrediente'
             });
         });
+}
+
+// Definir la función para eliminar un ingrediente del array
+function deleteIngredienteRecetas_nuevos(id) {
+    // Encontrar el índice del ingrediente en el array
+    const index = ingredientesSeleccionados.value.findIndex(ingrediente => ingrediente.id === id);
+    
+    // Si se encuentra el ingrediente en el array, eliminarlo
+    if (index !== -1) {
+        ingredientesSeleccionados.value.splice(index, 1); // Elimina el elemento en el índice "index"
+        // Aquí puedes agregar cualquier otra lógica que necesites después de eliminar el ingrediente
+    }
 }
 
 
